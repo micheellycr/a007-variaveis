1b9554fc4cfb6ea2dcf6aaa821f6e1427018b44e
unknown <micheellycr@gmail.com>
2022-07-19 21:58:29 -0300

    exercicio a007

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+const pergunta1 = "Você me conhece?"
+const pergunta2 = "Você gosta de mim?"
+const pergunta3 = "você quer ser minha amiga?"
+
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, "=" ,resposta1)
+console.log(pergunta2, "=" ,resposta2)
+console.log(pergunta3, "=" ,resposta3) 
+
